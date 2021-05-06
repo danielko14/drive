485de53c55227d9ea6be92a55a0e7d24858e175f
Daniel Ko <kod3349@gmail.com>
2021-05-06 14:15:30 -0500

    Add road and ground planes

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ let renderer;
 let scene;
 let car;
 let keyboard;
+let road;
+let ground;
+let xSpeed = 1;
+let ySpeed = 1;
 
 function init(){
   container = document.querySelector('.scene');
@@ -19,14 +23,14 @@ function init(){
 
   // Camera Setup
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 4, 13);
+  camera.position.set(0, 2.5, 12);
   scene.add(camera);
 
-  const ambient = new THREE.AmbientLight(0x404040, 2);
+  const ambient = new THREE.AmbientLight(0x404040, 1);
   scene.add(ambient);
 
   const light = new THREE.DirectionalLight(0xffffff, 3);
-  light.position.set(0, 0, 50);
+  light.position.set(0, 0, 10);
   scene.add(light);
 
   // Renderer
@@ -36,14 +40,23 @@ function init(){
 
   container.appendChild(renderer.domElement);
 
-  // Create Floor
-  let geometry = new THREE.PlaneGeometry(100, 50);
-  let material = new THREE.MeshBasicMaterial({color: 0x404040});
-  let floor = new THREE.Mesh(geometry, material);
-  floor.position.y = -1.9;
-  floor.rotation.x = 90;
-  floor.material.side = THREE.DoubleSide;
-  scene.add(floor);
+  // Create Road
+  let roadGeometry = new THREE.PlaneGeometry(10, 5000);
+  let roadMaterial = new THREE.MeshBasicMaterial({color: 0x404040});
+  road = new THREE.Mesh(roadGeometry, roadMaterial);
+  road.position.y = -1;
+  road.rotation.x = -Math.PI/2;
+  road.material.side = THREE.DoubleSide;
+  scene.add(road);
+
+  // Create Ground
+  let groundGeometry = new THREE.PlaneGeometry(1000, 5000);
+  let groundMaterial = new THREE.MeshBasicMaterial({color: 0xFFFFFF});
+  ground = new THREE.Mesh(groundGeometry, groundMaterial);
+  ground.position.y = -1.1;
+  ground.rotation.x = -Math.PI/2;
+  ground.material.side = THREE.DoubleSide;
+  scene.add(ground);
 
   // Controller
 
@@ -59,13 +72,38 @@ init();
 
 function animate() {
   requestAnimationFrame(animate);
-  //car.rotation.z += 0.005;
+  document.addEventListener("keydown", keyControls, false);
   renderer.render(scene, camera);
 }
 
-const keyControls = () => {
+const keyControls = (e) => {
   // Create keys to move GLTF object
+  const keys = {
+    WASD_Left: 65,
+    WASD_Right: 68,
+    WASD_Up: 87,
+    WASD_Down: 83
+  }
+
+  switch (e.keyCode) {
+    case keys.WASD_Left:
+      car.position.x -= 1;
+      break;
+    case keys.WASD_Right:
+      car.position.x += 1;
+      break;
+    case keys.WASD_Up:
+      car.position.z -= 2;
+      camera.position.z -= 2;
+      break;
+    case keys.WASD_Down:
+      car.position.z += 2;
+      camera.position.z += 2;
+      break;
+  }
 }
+
+// function that follows car around
 
 function onWindowResize() {
   camera.aspect = container.clientWidth / container.clientHeight;

861a8cccb1aafc226dc36607703bbe029a871c57
Daniel Ko <kod3349@gmail.com>
2021-05-06 13:04:12 -0500

    Added floor plane and star background

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let camera;
 let renderer;
 let scene;
 let car;
+let keyboard;
 
 function init(){
   container = document.querySelector('.scene');
@@ -11,14 +12,15 @@ function init(){
   // Create scene
   scene = new THREE.Scene();
 
-  const fov = 35;
+  const fov = 50;
   const aspect = container.clientWidth / container.clientHeight;
   const near = 0.1;
-  const far = 500;
+  const far = 10000;
 
   // Camera Setup
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 1, 12);
+  camera.position.set(0, 4, 13);
+  scene.add(camera);
 
   const ambient = new THREE.AmbientLight(0x404040, 2);
   scene.add(ambient);
@@ -34,6 +36,17 @@ function init(){
 
   container.appendChild(renderer.domElement);
 
+  // Create Floor
+  let geometry = new THREE.PlaneGeometry(100, 50);
+  let material = new THREE.MeshBasicMaterial({color: 0x404040});
+  let floor = new THREE.Mesh(geometry, material);
+  floor.position.y = -1.9;
+  floor.rotation.x = 90;
+  floor.material.side = THREE.DoubleSide;
+  scene.add(floor);
+
+  // Controller
+
   // Load model
   let loader = new THREE.GLTFLoader();
   loader.load('/porsche_911_rauh-welt/scene.gltf', (gltf) => {
@@ -42,20 +55,22 @@ function init(){
     animate();
   });
 }
+init();
 
 function animate() {
   requestAnimationFrame(animate);
-  car.rotation.z += 0.005;
+  //car.rotation.z += 0.005;
   renderer.render(scene, camera);
+}
+
+const keyControls = () => {
+  // Create keys to move GLTF object
 }
 
 function onWindowResize() {
   camera.aspect = container.clientWidth / container.clientHeight;
   camera.updateProjectionMatrix();
-
   renderer.setSize(container.clientWidth, container.clientHeight);
 }
 
-init();
-
-window.addEventListneer("resize", onWindowResize);
+window.addEventListener("resize", onWindowResize);
